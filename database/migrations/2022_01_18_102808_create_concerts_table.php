536d39d985c02e4f6ba8cc2722a75182164b3c46
GU NA HYUN <80106663+agunacoco@users.noreply.github.com>
2022-01-18 20:09:09 +0900

    0118laravel migration

---
+++ database/migrations/2022_01_18_102808_create_concerts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConcertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('concerts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->date('startDate');
+            $table->date('endDate');
+            $table->string('title');
+            $table->string('desc');
+            $table->longText('content');
+            $table->string('artist');
+            $table->bigInteger('price');
+            $table->unsignedBigInteger('remainTicket');
+            $table->date('openDate');
+            $table->date('closeDate');
+            $table->string('playTime');
+            $table->date('reEndDate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('concerts');
+    }
+}

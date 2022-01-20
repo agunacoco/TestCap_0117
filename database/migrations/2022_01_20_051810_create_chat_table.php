329763e91eb7305966fd3ab18a8e26fb7265d48c
GU NA HYUN <80106663+agunacoco@users.noreply.github.com>
2022-01-20 15:15:28 +0900

    0120laravel migrate

---
+++ database/migrations/2022_01_20_051810_create_chat_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat', function (Blueprint $table) {
+            $table->id();
+            $table->string('chatContent')->nullable();
+            $table->index('user_id');
+            $table->index('concert_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat');
+    }
+}
---
+++ database/migrations/2022_01_20_051837_create_coin_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoinTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coin', function (Blueprint $table) {
+            $table->id();
+            $table->index('user_id');
+            $table->index('concert_id')->nullable();
+            $table->index('chat_id')->nullable();
+            $table->string('cHistory');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coin');
+    }
+}
